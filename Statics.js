2656c5882d829680ca1c61be505ecb53ba943f89
Pastaguy1 <125645624+Pastaguy1@users.noreply.github.com>
2024-09-12 12:17:19 +0200

    statics.js

---
+++ Statics.js
@@ -0,0 +1,25 @@
+let name = "Spiderman";
+console.log("Name: "+ name);
+let jumpforce = 30;
+console.log("jumpforce: " + jumpforce);
+let specialpower = "spiderwebs";
+console.log("Special power: " + specialpower);
+let speed = 250;
+console.log("Speed: " + speed);
+let bike = "Kawasaki ninja";
+console.log("Bike: " + bike);
+let lives = 2;
+console.log("Lives left: "+ lives);
+let ammo = 154;
+console.log("Ammo: "+ ammo);
+let webs = 23;
+console.log("Number of webs: " + webs);
+let battery = 20;
+console.log("Battery power: " + battery);
+let level = 45;
+console.log("level: " + level); 
+let wanted = "Not wanted";
+console.log("wanted: " + wanted);
+
+
+
