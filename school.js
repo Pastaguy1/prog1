9cece790218b818677a85ea14709d2c8bb20c03c
Pastaguy1 <125645624+Pastaguy1@users.noreply.github.com>
2024-09-19 11:46:43 +0200

    Add files via upload

--- school.js
+++ school.js
@@ -1,11 +1,8 @@
 const vandaag = new Date();
 const huidigeDag = vandaag.getDay();
 
-
-if(huidigeDag === 0) {
-    console.log("zondag dus lekker uitslapen");
-} else if (huidigeDag === 6) {
-    console.log("Lekker uitslapen het is zaterdag!");
-} else {
-    console.log("Schooldag, yey");
+if(huidigeDag == 0 || huidigeDag == 6 ) {
+    console.log("weekend dus lekker uitslapen");
+}  else {
+       console.log("Schooldag, yey");
 }   
