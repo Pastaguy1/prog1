752d3df72237b6246fe417f91c3740b6a7a031fb
Pastaguy1 <125645624+Pastaguy1@users.noreply.github.com>
2024-10-09 16:12:27 +0200

    Add files via upload

--- Tafels/tafels.js
+++ Tafels/tafels.js
@@ -1,5 +1,4 @@
-for(let t = 1; t <= 50; t++){
-    for(let k = 1; k <= 50; k++) {
-        console.log(t + "*" + k + "=" + (t * k));
-    }
+for(let i = 1; i <= 50; i++){
+    for(let j = 1; j <= 50; j++)
+    console.log(i * j)
 }
